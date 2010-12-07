ead2177c68e1c223fea768ed5764b3825020f33d
Jon Druse <jon@jondruse.com>
2010-12-07 10:34:15 -0800

    keep track of our current place for dimensions also

--- lib/star_etl/dimension_factory.rb
+++ lib/star_etl/dimension_factory.rb
@@ -4,7 +4,8 @@ module StarEtl
     attr_accessor :source, :sources, :dimensions
     
     def initialize
-      @dimensions = {}
+      @primary_key = StarEtl.options[:primary_key]
+      @dimensions  = {}
     end
     
     def []=(k,v)
@@ -13,12 +14,16 @@ module StarEtl
     
     def run!
       [@source,@sources].flatten.compact.each do |source|
+        
+        get_id_range("#{source.gsub("\"",'')}_dimension")
+        
         @dimensions.each_pair do |name, config|
           puts "Synchronizing #{name} from #{source}"
           
           opts = config.clone
           
           conditions = ["dimension IS NULL"]
+          conditions << @id_range.call
           conditions << opts.delete(:conditions)
           join = opts.delete(:join)
           group = opts.delete(:group)
@@ -42,6 +47,30 @@ module StarEtl
       end
 
     end
+    
+    def get_id_range(source)
+      get_last_id(source)
+      @nothing_new = true if @last_id.to_i == @_to_id_.to_i
+      @id_range = lambda {"source.#{@primary_key} BETWEEN #{@last_id} AND #{@_to_id_}"}
+    end
+    
+    private
+    
+    def get_last_id(source)
+      info = sql(%Q{SELECT * from etl_info WHERE table_name = '#{source}' })
+      @last_id = if info.empty?
+        sql(%Q{INSERT INTO etl_info (last_id, table_name) VALUES (0, '#{source}') })
+        0
+      else
+        info.first["last_id"]
+      end
+      @_to_id_ = sql(%Q{SELECT max(#{@primary_key}) as "max" FROM #{source}}).first["max"]
+      
+      if @last_id && @_to_id_
+        sql(%Q{UPDATE etl_info SET last_id = #{@_to_id_} WHERE table_name = '#{source}'})
+      end
+    end
+    
     
   end
 end
